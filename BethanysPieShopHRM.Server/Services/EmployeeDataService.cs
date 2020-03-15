cb23529b8c7db704446d79008f5c7ea7c5b849b5
andy <andy.bochmann@gmail.com>
2020-03-15 16:02:42 -0700

    Edit employee form

--- BethanysPieShopHRM.Server/Services/EmployeeDataService.cs
+++ BethanysPieShopHRM.Server/Services/EmployeeDataService.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 
@@ -31,19 +32,30 @@ namespace BethanysPieShopHRM.Server.Services
                     { PropertyNameCaseInsensitive = true });
         }
 
-        public Task<Employee> AddEmployee(Employee employee)
+        public async Task<Employee> AddEmployee(Employee employee)
         {
-            throw new NotImplementedException();
+            var employeeJson = new StringContent(JsonSerializer.Serialize(employee), Encoding.UTF8, "application/json");
+
+            var response = await _httpClient.PostAsync("api/employee", employeeJson);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return await JsonSerializer.DeserializeAsync<Employee>(await response.Content.ReadAsStreamAsync());
+            }
+
+            return null;
         }
 
-        public Task DeleteEmployee(int employeeId)
+        public async Task UpdateEmployee(Employee employee)
         {
-            throw new NotImplementedException();
+            var employeeJson = new StringContent(JsonSerializer.Serialize(employee), Encoding.UTF8, "application/json");
+
+            await _httpClient.PutAsync("api/employee", employeeJson);
         }
 
-        public Task UpdateEmployee(Employee employee)
+        public async Task DeleteEmployee(int employeeId)
         {
-            throw new NotImplementedException();
+            await _httpClient.DeleteAsync($"api/employee/{employeeId}");
         }
     }
 }
