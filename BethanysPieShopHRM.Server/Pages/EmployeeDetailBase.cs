e0d538fb9ca0e8c12b7f4a3005be29b06fd47004
andy <andy.bochmann@gmail.com>
2020-04-11 21:53:40 -0700

    Add Map

--- BethanysPieShopHRM.Server/Pages/EmployeeDetailBase.cs
+++ BethanysPieShopHRM.Server/Pages/EmployeeDetailBase.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BethanysPieShopHRM.ComponentsLibrary.Map;
 using BethanysPieShopHRM.Server.Services;
 using BethanysPieShopHRM.Shared;
 using Microsoft.AspNetCore.Components;
@@ -13,14 +14,26 @@ namespace BethanysPieShopHRM.Server.Pages
         [Inject]
         public IEmployeeDataService EmployeeDataService { get; set; }
 
-        [Parameter] 
+        [Parameter]
         public string EmployeeId { get; set; }
 
         public Employee Employee { get; set; } = new Employee();
 
+        public List<Marker> MapMarkers { get; set; } = new List<Marker>();
+
         protected override async Task OnInitializedAsync()
         {
             Employee = await EmployeeDataService.GetEmployeeDetails(int.Parse(EmployeeId));
+
+            MapMarkers = new List<Marker>
+            {
+                new Marker {
+                    Description = $"{Employee.FirstName} {Employee.LastName}",
+                    ShowPopup = false,
+                    Y = Employee.Latitude,
+                    X = Employee.Longitude 
+                }
+            };
         }
 
         public IEnumerable<Employee> Employees { get; set; }
